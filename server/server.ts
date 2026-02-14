5365d864ba19286a8f28220f15da09999a819963
Moses <moseschizaram8@gmail.com>
2026-02-14 10:11:46 +0100

    changed cors permission to automatic

--- server/server.ts
+++ server/server.ts
@@ -107,34 +107,7 @@ export const createApp = (): { app: Express; httpServer: HttpServer } => {
     })
   );
 
-  // Manual CORS headers for all requests
-  // app.use((req, res, next) => {
-  //   res.header('Access-Control-Allow-Origin', '*');
-  //   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  //   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  //   res.header('Access-Control-Allow-Credentials', 'true');
-    
-  //   if (req.method === 'OPTIONS') {
-  //     return res.status(200).end();
-  //   }
-  //   next();
-  // });
-
-  // Configure CORS with specific options
-  const corsOptions = {
-    origin:
-      process.env.NODE_ENV === 'production'
-        ? true
-        : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'https://wccrm-lagos.vercel.app'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    optionsSuccessStatus: 200,
-  };
-
-  // Apply CORS with the specified options
-  app.use(cors(corsOptions));
-  
+ 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
