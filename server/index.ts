074200609e53683b9cf244e63a8fd8daa17c53d3
Moses <moseschizaram8@gmail.com>
2026-02-14 11:01:25 +0100

    Add CORS middleware to index.ts

--- server/index.ts
+++ server/index.ts
@@ -14,6 +14,18 @@ declare module "http" {
   }
 }
 
+// CORS - allow all origins
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  next();
+});
+
 app.use(
   express.json({
     verify: (req, _res, buf) => {
